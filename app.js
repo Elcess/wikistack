00e1d2af5f3047f28dbacee18a16f4bf2e51d29c
Elcess <kelcess@alum.mit.edu>
2018-11-09 12:34:06 -0500

    add Page and User tables

--- app.js
+++ app.js
@@ -5,21 +5,33 @@ const pg = require('pg');
 const Sequelize = require('sequelize');
 const bodyParser = require('body-parser');
 const layout = require('./views/layout');
+const models = require('./models');
 
+// models.authenticate().then(() => {
+//   console.log('connected to the database');
+// });
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-
   res.send(layout(''));
 });
 
-
-
-
-
 const Port = 3000;
-app.listen(Port);
+const init = async () => {
+  try {
+    await models.Page.sync();
+    await models.User.sync();
+
+    app.listen(Port, () => {
+      console.log(`Server is listening on port ${Port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+init();
