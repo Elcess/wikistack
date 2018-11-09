5049e0b6a1270ec1bf390c0b020955244a1a2168
Elcess <kelcess@alum.mit.edu>
2018-11-09 15:15:38 -0500

    add addPage functionality

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,24 +1,37 @@
 const router = require('express').Router();
-const main = require('../views/main');
-const addPage = require('../views/addPage');
-
-
+const { Page } = require('../models');
+const { addPage } = require('../views');
 
 router.get('/', async (req, res, next) => {
-
-  res.send('GET/hello');
+  res.redirect('/');
   // res.send(main(''));
-
 });
 
+const generateSlug = title => {
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+};
+
 router.post('/', async (req, res, next) => {
-  res.send('POST/hello');
-  // res.send(addPage());
+  const title = req.body.title;
+  const content = req.body.content;
+  const status = req.body.status;
+  const page = new Page({
+    title: title,
+    content: content,
+    slug: slug,
+    status: status,
+  });
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/add', async (req, res, next) => {
-  res.send('GET/add hello');
+  res.send(addPage());
   // res.send(addPage());
-})
+});
 
 module.exports = router;
